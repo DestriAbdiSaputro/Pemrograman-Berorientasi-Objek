bcd5702923a0f1df4ba6a1d897abd8a8897e71d4
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-10-10 21:13:20 +0700

    Create Persegi.java

---
+++ PBO-04/ProjectBangunDatar2/src/projectbangundatar2/Persegi.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Persegi {
+    void Persegi(int s){
+        int luas;
+        int keliling;
+        luas = s*s;
+        keliling = 4*s;
+        System.out.println("Luas Persegi : "+luas);
+        System.out.println("Keliling Persegi : "+keliling);
+    }
+    
+    void Persegi(double s){
+        double luas;
+        double keliling;
+        luas = s*s;
+        keliling = 4*s;
+        System.out.println("Luas Persegi : "+luas);
+        System.out.println("Keliling Persegi : "+keliling);
+    }
+}
