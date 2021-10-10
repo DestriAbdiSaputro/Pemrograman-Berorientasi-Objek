1ae61ad62f3a1519cf363ebe5679c5925468841e
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-10-10 21:13:21 +0700

    Create PersegiPanjang.java

---
+++ PBO-04/ProjectBangunDatar2/src/projectbangundatar2/PersegiPanjang.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class PersegiPanjang {
+    void PPanjang(int p, int l){
+        int luas;
+        int keliling;
+        luas = p*l;
+        keliling = 2*(p+l);
+        System.out.println("Luas Persegi Panjang : "+luas);
+        System.out.println("Keliling Persegi Panjang : "+keliling);
+    }
+    
+    void PPanjang(double p, int l){
+        double luas;
+        double keliling;
+        luas = p*l;
+        keliling = 2*(p+l);
+        System.out.println("Luas Persegi Panjang : "+luas);
+        System.out.println("Keliling Persegi Panjang : "+keliling);
+    }
+    
+    void PPanjang(int p, double l){
+        double luas;
+        double keliling;
+        luas = p*l;
+        keliling = 2*(p+l);
+        System.out.println("Luas Persegi Panjang : "+luas);
+        System.out.println("Keliling Persegi Panjang : "+keliling);
+    }
+    
+    void PPanjang(double p, double l){
+        double luas;
+        double keliling;
+        luas = p*l;
+        keliling = 2*(p+l);
+        System.out.println("Luas Persegi Panjang : "+luas);
+        System.out.println("Keliling Persegi Panjang : "+keliling);
+    }
+}
