4d31fd0da565f8ae8bb4e6bc4ae050c75c8384a1
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-10-10 21:13:17 +0700

    Create Lingkaran.java

---
+++ PBO-04/ProjectBangunDatar2/src/projectbangundatar2/Lingkaran.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectbangundatar2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Lingkaran {
+    void Lingkaran(int r){
+        int luas;
+        int keliling;
+        luas = 22/7*r*r;
+        keliling = 22/7*r*r;
+        System.out.println("Luas Lingkaran : "+luas);
+        System.out.println("Keliling Lingkaran : "+keliling);
+    }
+    
+    void Lingkaran(double r){
+        double luas;
+        double keliling;
+        luas = 22/7*r*r;
+        keliling = 22/7*r*r;
+        System.out.println("Luas Lingkaran : "+luas);
+        System.out.println("Keliling Lingkaran : "+keliling);
+    }
+}
