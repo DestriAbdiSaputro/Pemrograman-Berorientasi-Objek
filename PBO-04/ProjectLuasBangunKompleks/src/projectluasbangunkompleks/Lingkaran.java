ea36a5471a2559d123d441e73944d19956bbafae
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-10-10 21:13:39 +0700

    Create Lingkaran.java

---
+++ PBO-04/ProjectLuasBangunKompleks/src/projectluasbangunkompleks/Lingkaran.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectluasbangunkompleks;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Lingkaran {
+    int r;
+    
+    //method
+    int hitungLuas(){
+        int luas = 22/7*r*r;
+        return luas;
+    }
+}
