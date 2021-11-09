fba8942d9aad84e9a9adb13aef52a26e875db637
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-11-09 22:33:32 +0700

    Create MinMax.java

---
+++ ProjectRerata/src/ArrayProcessing/MinMax.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ArrayProcessing;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MinMax {
+    public static void main(String[] args){
+        ArrayProcessing r = new ArrayProcessing();
+        r.input();
+        r.output();
+    }
+}
