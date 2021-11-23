95bcdebd7413d112b00cb787ffc982cdb4cf17df
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-11-23 21:32:52 +0700

    Create RentalConsole.java

---
+++ RentalConsole/src/rentalconsole/RentalConsole.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalconsole;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class RentalConsole {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Member membership = new Member();
+        membership.tambahMember(new DataMember("M001", "Mr. X", "Silver"));
+        membership.tambahMember(new DataMember("M002", "Mr. Y", "Gold"));
+        membership.tambahMember(new DataMember("M003", "Mr. Z", "Platinum"));
+        DataMember member = membership.inputMember();
+        switch(member.jenis) {
+            case "Silver":
+                Silver silver = new Silver(member);
+                silver.calculatedServices();
+                silver.output();
+                break;
+            case "Gold":
+                Gold gold = new Gold(member);
+                gold.calculatedServices();
+                gold.output();
+                break;
+            case "Platinum":
+                Platinum platinum = new Platinum(member);
+                platinum.calculatedServices();
+                platinum.output();
+                break;
+        }      
+    }
+}
