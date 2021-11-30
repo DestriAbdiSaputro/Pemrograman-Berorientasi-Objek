06611bd94d82f836db976edca7985d57b6df3b5a
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-11-30 21:46:28 +0700

    Create Gold.java

---
+++ RentalConsoleImprove/src/rentalconsoleimpove/Gold.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rentalconsoleimpove;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Gold extends Member {
+    int diskon = 2;
+    int poin;
+    int totalSewa;
+    int cashback =  5000;
+    
+    
+    public Gold(DataMember member) {
+        super.member = member;
+        super.biayaSewa = 30000;
+        super.inputPeminjaman();
+    }
+    
+    public void hitung() {
+        this.poin = 5 * selisihHari;
+        this.totalSewa = this.biayaSewa * this.selisihHari;
+        int diskon = this.totalSewa * this.diskon/100;
+        this.totalSewa -= diskon;
+    }
+    
+    @Override
+    protected void output() {
+        super.output();
+        System.out.println("Total Sewa		: " + this.totalSewa);
+        System.out.println("Jumlah Poin		: " + this.poin);
+        System.out.println("Jumlah Cashback     : " + this.cashback);
+    }
+}
