ce97ddb67dca9c4d3d8a956525f7a583570de72d
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-12-07 20:07:05 +0700

    Create MPV.java

---
+++ ProjectMobil/src/mobil/MPV.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mobil;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class MPV extends hitungPerjalanan{
+    @Override
+    protected double bahanBakar(){
+        return 14.;
+    }
+}
