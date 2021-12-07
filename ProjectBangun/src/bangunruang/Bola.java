337cc22625e08cec61907fe1d7cabaa922ef8c75
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-12-07 20:06:37 +0700

    Create Bola.java

---
+++ ProjectBangun/src/bangunruang/Bola.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bangunruang;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Bola extends BangunRuang {
+    public double jari;
+    
+    @Override
+    public double hitungVolume(){
+        return (4./3.) * Math.PI * this.jari * this.jari * this.jari;
+    }
+    
+    @Override
+    public double hitungLuas(){
+        return 4*Math.PI*this.jari*this.jari;
+    }
+}
