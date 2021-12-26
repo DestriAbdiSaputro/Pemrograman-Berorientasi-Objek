6bbf20868c57762106b249c51b26e156c18535dd
Destri Abdi Saputro <destriabdi20@gmail.com>
2021-12-26 23:26:13 +0700

    Create C.java

---
+++ ProjectMandiri/src/golongan/C.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package golongan;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class C extends Golongan {    
+    
+    public C(String status, int usia, int anak) {
+        super(status, usia, anak);
+    }
+    @Override
+    protected double gajiPokok(){
+        return this.gjPokok = 7000000.;
+    }
+}
